411ee0ee97e4baef59e5d0dbe13b713bd4ee582a
William Grzybowski <william@grzy.org>
2022-01-16 09:10:55 -0300

    Add push_transaction endpoint to wallet RPC client

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -127,6 +127,7 @@ class WalletRpcApi:
             "/get_next_address": self.get_next_address,
             "/send_transaction": self.send_transaction,
             "/send_transaction_multi": self.send_transaction_multi,
+            "/push_transaction": self.push_transaction,
             "/get_farmed_amount": self.get_farmed_amount,
             "/create_signed_transaction": self.create_signed_transaction,
             "/delete_unconfirmed_transactions": self.delete_unconfirmed_transactions,
@@ -976,6 +977,21 @@ class WalletRpcApi:
 
         # Transaction may not have been included in the mempool yet. Use get_transaction to check.
         return {"transaction": transaction, "transaction_id": tr.name}
+
+    async def push_transaction(self, request):
+        assert self.service.wallet_state_manager is not None
+
+        if await self.service.wallet_state_manager.synced() is False:
+            raise ValueError("Wallet needs to be fully synced before sending transactions")
+
+        wallet_id = uint32(request["wallet_id"])
+        wallet = self.service.wallet_state_manager.wallets[wallet_id]
+        tx = TransactionRecord.from_json_dict(request["transaction"])
+
+        async with self.service.wallet_state_manager.lock:
+            await wallet.push_transaction(tx)
+
+        return {"success": True}
 
     async def delete_unconfirmed_transactions(self, request) -> EndpointResult:
         wallet_id = uint32(request["wallet_id"])
