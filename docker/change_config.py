7913281950956863af6e103406c52932cbf2efaa
William Grzybowski <william@grzy.org>
2022-03-08 13:27:45 -0300

    Docker fixes

--- docker/change_config.py
+++ docker/change_config.py
@@ -10,15 +10,29 @@ def main():
         config = yaml.safe_load(f)
 
     chia_network = os.environ.get('CHIA_NETWORK', 'mainnet')
-    if 'testnet' in chia_network:
-        default_node_port = 58444
-    else:
-        default_node_port = 8444
+
+    log_level = os.environ.get('CHIA_LOGLEVEL', 'WARNING')
+    config['farmer']['logging']['log_level'] = log_level
+
+    multiprocessing_method = os.environ.get('CHIA_MULTIPROCESSING_METHOD', 'default')
+    config['wallet']['multiprocessing_start_method'] = multiprocessing_method
+
+    trusted_node_id = os.environ.get('CHIA_TRUSTED_NODEID') or 'trusted_node_1'
 
     config['self_hostname'] = '0.0.0.0'
-    config['wallet']['full_node_peer']['host'] = os.environ.get('CHIA_NODE_HOST', 'localhost')
-    config['wallet']['full_node_peer']['port'] = int(os.environ.get('CHIA_NODE_PORT', default_node_port))
-    config['wallet']['trusted_peers']['trusted_node_1'] = os.environ.get('CHIA_NODE_CRT', f'/data/chia/{chia_network}/config/ssl/full_node/public_full_node.crt')
+
+    node_host = os.environ.get('CHIA_NODE_HOST')
+    if node_host:
+        if 'testnet' in chia_network:
+            default_node_port = 58444
+        else:
+            default_node_port = 8444
+
+        config['wallet']['full_node_peer']['host'] = node_host
+        config['wallet']['full_node_peer']['port'] = int(os.environ.get('CHIA_NODE_PORT', default_node_port))
+    else:
+        config['wallet'].pop('full_node_peer', None)
+    config['wallet']['trusted_peers'][trusted_node_id] = os.environ.get('CHIA_NODE_CRT', f'/data/chia/{chia_network}/config/ssl/full_node/public_full_node.crt')
 
     with open(CONFIG_PATH, 'w') as f:
         yaml.dump(config, f)
